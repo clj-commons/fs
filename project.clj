b48499993107682a02cd04c67bf2c5207ad28ebf
Erik Assum <erik@assum.net>
2018-11-14 20:59:48 +0100

    Reflect move to clj-commons

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "File system utilities for clojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/Raynes/fs"
+  :url "https://github.com/clj-commons/fs"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.commons/commons-compress "1.8"]]
   :plugins [[lein-midje "3.1.3"]
             [codox "0.8.10"]]
-  :codox {:src-dir-uri "https://github.com/Raynes/fs/blob/master/"
+  :codox {:src-dir-uri "https://github.com/clj-commons/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
   :deploy-repositories [["releases" {:url           "https://clojars.org/repo"
