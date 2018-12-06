64f1a3bf5f6679f077d8de97ac8527ecafd72263
Erik Assum <erik@assum.net>
2018-12-07 00:00:11 +0100

    Upgrade libs

--- project.clj
+++ project.clj
@@ -6,9 +6,10 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.commons/commons-compress "1.8"]]
   :plugins [[lein-midje "3.1.3"]
-            [codox "0.8.10"]]
+            [codox "0.8.10"]
+            [lein-ancient "0.6.15"]]
   :codox {:src-dir-uri "https://github.com/clj-commons/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}
+  :profiles {:dev {:dependencies [[midje "1.9.4"]]}
              :set-version {:plugins [[lein-set-version "0.4.1"]]}})
