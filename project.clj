c6e17b6e4a87cc90c1e7a53b0f64bc43ba0969c5
Erik Assum <erik@assum.net>
2018-12-06 23:50:04 +0100

    Prepare for release
    - Change artefact coords to reflect move to clj-commons
    - Upgrade Clojure to most recent released version
    - Update version to be first minor version after Raynes latest release, 1.4.6

--- project.clj
+++ project.clj
@@ -1,18 +1,14 @@
-(defproject akvo/fs "dev"
+(defproject clj-commons/fs "1.5.0-SNAPSHOT"
   :description "File system utilities for clojure"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/clj-commons/fs"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.commons/commons-compress "1.8"]]
   :plugins [[lein-midje "3.1.3"]
             [codox "0.8.10"]]
   :codox {:src-dir-uri "https://github.com/clj-commons/fs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :defaults {:doc/format :markdown}}
-  :deploy-repositories [["releases" {:url           "https://clojars.org/repo"
-                                     :sign-releases false
-                                     :username      "akvo"
-                                     :password      :env/clojars_password}]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}
              :set-version {:plugins [[lein-set-version "0.4.1"]]}})
