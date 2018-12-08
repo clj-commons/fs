265a70ce0c87dbf337c0815a6931110005f1d960
Erik Assum <erik@assum.net>
2018-12-08 19:46:02 +0100

    Use Circle for builds

--- ci/release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-RELEASE_VERSION="$(date "+%Y%m%d-%H%M%S").${TRAVIS_COMMIT:0:8}"
-
-lein with-profile +set-version set-version $RELEASE_VERSION
-
-lein deploy releases
